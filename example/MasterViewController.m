8ff2dcc4821ca631252c6692928ea996a2504f72
Zach Dennis <zach.dennis@gmail.com>
2013-11-14 10:49:54 -0500

    Beacons are now to the tableview when they are found as opposed to when the nearest beacon changes.

--- example/MasterViewController.m
+++ example/MasterViewController.m
@@ -28,9 +28,10 @@
 {
     [super viewDidLoad];
 
+    _objects = [[NSMutableArray alloc] init];
+
     [[GeLoCache sharedCache] loadSite:[NSNumber numberWithInt:3]];
     [[GeLoCache sharedCache] loadTours];
-
 
     [[GeLoBeaconManager sharedInstance] loadTourById:[NSNumber numberWithInt:22] ];
     [[GeLoBeaconManager sharedInstance] loadSiteById:[NSNumber numberWithInt:3 ] ];
@@ -38,6 +39,7 @@
 - (void)viewWillAppear:(BOOL)animated{
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(foundBeacon:) name:kGeLoBeaconFound object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(nearestBeacon:) name:kGeLoNearestBeaconChanged object:nil];
+
     [[GeLoBeaconManager sharedInstance] startScanningForBeacons];
     NSLog(@"Scanning for Beacons!!!");
 }
@@ -48,36 +50,32 @@
     NSLog(@"Stop the Scanning!!!");
 }
 
-- (void)foundBeacon:(NSNotification *)sender {
-    // do something with the discovered beacon here
-    GeLoBeacon *beacon = sender.userInfo[@"beacon"];
-    NSString *str = [NSString stringWithFormat:@"%d",[beacon beaconId]];
-    NSLog(@"Found Beacon : %@",str);
-}
-
-- (void)nearestBeacon:(NSNotification *)sender {
-    GeLoBeacon *beacon = sender.userInfo[@"beacon"];
-    NSString *str = [NSString stringWithFormat:@"%d",[beacon beaconId]];
+- (void) addBeacon: (GeLoBeacon *) beacon {
     GeLoBeaconInfo *beaconInfo = [beacon info];
-    NSLog(@"Nearest Beacon Changed : %@",str);
-    
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    
-    // if we have a non-nil beacon with beaconInfo that's present
     if ([[beaconInfo name] length]){
         // and it's not already in our list
         if ([_objects indexOfObject:beacon] == NSNotFound) {
             [_objects insertObject:beacon atIndex:0];
             NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+
             [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
         }
     }
-    
+}
 
+- (void)foundBeacon:(NSNotification *)sender {
+    GeLoBeacon *beacon = sender.userInfo[@"beacon"];
+    NSString *str = [NSString stringWithFormat:@"%lu", (unsigned long)[beacon beaconId]];
+    NSLog(@"Found Beacon : %@", str);
 
-    
+    // do something with the discovered beacon here
+    [self performSelectorInBackground:@selector(addBeacon:) withObject:beacon];
+}
+
+- (void)nearestBeacon:(NSNotification *)sender {
+    GeLoBeacon *beacon = sender.userInfo[@"beacon"];
+    NSString *str = [NSString stringWithFormat:@"%lu", (unsigned long)[beacon beaconId]];
+    NSLog(@"Nearest Beacon Changed : %@",str);
 }
 
 - (void)didReceiveMemoryWarning
@@ -87,6 +85,7 @@
 }
 
 #pragma mark - Table View
+
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
